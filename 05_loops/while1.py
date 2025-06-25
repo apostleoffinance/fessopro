b41b078bd3ed8511c95ea5306c876e13887feaa7
Olaoluwa Tunmise <oluwatunmise1olaoluwa@gmail.com>
2025-06-25 16:07:56 +0100

    while

---
+++ 05_loops/while1.py
@@ -0,0 +1,29 @@
+i = 10
+while True:
+    if i==20:
+        break
+    print(i)
+    i=i+1
+
+
+# Get even numbers between 1 and 100
+
+#i = 100
+even = []
+while True:
+   if i>100:
+       break
+   i=i+1
+   if i%2==0:
+       even.append(i)
+   print(i)
+   
+
+#Multiplication Table 3 x 3 = 9
+num1 = 3
+num2 = 1
+while True:
+    if num2==11:
+        break
+    print(num1, 'X',num2,'=', num1*num2)
+    num2=num2+1
