56f435736dc39b78a8292acc2cfbfd2b6d7effb3
Olaoluwa Tunmise <oluwatunmise1olaoluwa@gmail.com>
2025-06-25 23:56:10 +0100

    while

---
+++ 05_loops/while2.py
@@ -0,0 +1,40 @@
+# num=1
+
+# while True:
+#     if num==101:
+#         break
+#     print(num)
+#     num=num+2
+
+# print('last line')
+
+# stock_prices={'tsla':700,'goog':900,'amzn':680,'nvda':567}
+# portfolio=[]
+# while True:
+#     name=input('enter the stock name (q to quit)')
+#     if name.upper()=='Q':
+#         break
+#     if name=='nvda':
+#         print('you cannot trade this stock try something else')
+#         continue
+
+#     found=stock_prices.get(name)
+#     if found:
+#         portfolio.append(name)
+#     else:
+#         print('this stock name is invalid type again')
+# print(portfolio)
+
+list1 = [44,55,63,45,23,67,56]
+high=list1[0]
+i=0
+while True:
+    if i==len(list1):
+        break
+    print(list1[i])
+    if high<list1[i]:
+        high=list1[i]
+    i=i+1
+    
+print(high)
+
