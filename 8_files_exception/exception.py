32ed50434b9fb201c77419d0adb090bca51f3c33
Olaoluwa Tunmise <oluwatunmise1olaoluwa@gmail.com>
2025-06-25 12:14:00 +0100

    update

---
+++ 8_files_exception/exception.py
@@ -0,0 +1,6 @@
+"""Once upon a time,
+there was a brave knight,
+who fought dragons."""
+
+
+f1=open('/Users/olaoluwatunmise/fessopro/8_files_exception/story.txt')
---
+++ 8_files_exception/files1.py
@@ -0,0 +1,7 @@
+#We always store string in text file 
+
+a = 'tsla'
+
+f1=open('demo1.txt', 'w')
+f1.write(a)
+f1.close()
