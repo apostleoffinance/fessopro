9ba4e0e84449feb4ed5a3a70ccdf3789aabe5bfc
Olaoluwa Tunmise <oluwatunmise1olaoluwa@gmail.com>
2025-07-22 16:33:38 +0100

    updated

--- 08_files_exception/exception.py
+++ 08_files_exception/exception.py
@@ -2,5 +2,13 @@
 there was a brave knight,
 who fought dragons."""
 
+try:
+    f1=open('/Users/olaoluwatunmise/fessopro/8_files_exception/story.txt')
+    d = f1.read()
+    print(d)
+    print(100/0)
 
-f1=open('/Users/olaoluwatunmise/fessopro/8_files_exception/story.txt')
+except:
+    print('something went wrong')
+
+print('This is very important line')
