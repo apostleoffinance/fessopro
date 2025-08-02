7f85f73d8e136b1ce050f8bd21922917c60c0f78
Olaoluwa Tunmise <oluwatunmise1olaoluwa@gmail.com>
2025-08-02 12:27:37 +0100

    update

---
+++ 12_datetime_analysis/timezone1.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+time_zone='America/New_York'
+
+
+data = pd.read_csv('/Users/olaoluwatunmise/fessopro/12_datetime_analysis/data (2).csv')
+#drop the first two rows that are not needed
+data.drop([0,1], axis=0,inplace=True)
+
+#Rename column
+data.rename(columns={'Price':'Date'}, inplace=True)
+
+#convert date column from object to datetime
+data['Date']=pd.to_datetime(data['Date'])
+
+#convert to timezone
+data['Date'] = data['Date'].dt.tz_convert(time_zone)
+
+print(data)
+print(data.info())
